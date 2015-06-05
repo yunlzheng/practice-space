3e6f58aeb0dc47bb2a31bd9cc1cfcfb775e5e44d
yunl.zheng <yunl.zheng@gmail.com>
2015-06-05 22:07:00 +0800

    exercise 09

---
+++ learn-python-the-hard-way/exercise-09/ex9.py
@@ -0,0 +1,12 @@
+# There's some new strange stuff, remeber type it exactly
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFen\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days:", days
+print "Here are the months", months
+print """
+There's something going on here.
+With the theree double-quotes.
+We'll be able to type as much as we like.
+Eben 4 lines if we want, or 5, or 6
+"""
