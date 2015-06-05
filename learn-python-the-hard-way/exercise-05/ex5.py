3adcb8b3c18a5e3f1e4745fbfeb866c1eda647de
yunl.zheng <yunl.zheng@gmail.com>
2015-06-05 21:36:00 +0800

    exercise 05

---
+++ learn-python-the-hard-way/exercise-05/ex5.py
@@ -0,0 +1,12 @@
+my_name = "Zed A. Shaw"
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = "white"
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
