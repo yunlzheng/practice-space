0562ae62a4aee65147ebf4d968ed0b9bdd22785e
yunl.zheng <yunl.zheng@gmail.com>
2015-06-05 21:02:09 +0800

    exercise 01

---
+++ learn-python-the-hard-way/exercise-01/ex2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# A comment, this is so you can read your program later
+# Anything after the # is ignored by python
+
+print "I could have code like this." # And the coment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This is not run."
+
+print "This will run."
