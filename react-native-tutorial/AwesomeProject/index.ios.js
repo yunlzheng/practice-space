58dc4a5a0d46f0da3616b472a8c5579a252415b8
yunl.zheng <yunl.zheng@gmail.com>
2015-06-23 13:19:14 +0800

    Add some styling

--- react-native-tutorial/AwesomeProject/index.ios.js
+++ react-native-tutorial/AwesomeProject/index.ios.js
@@ -22,9 +22,13 @@ var AwesomeProject = React.createClass({
     var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
-        <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year} >{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -33,9 +37,21 @@ var AwesomeProject = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
   },
   welcome: {
     fontSize: 20,
