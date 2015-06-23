1b73d68a03ef86ead6d606b26abdb2791d8cab4e
yunl.zheng <yunl.zheng@gmail.com>
2015-06-23 13:18:44 +0800

    Render a movie

--- react-native-tutorial/AwesomeProject/index.ios.js
+++ react-native-tutorial/AwesomeProject/index.ios.js
@@ -4,28 +4,27 @@
  */
 'use strict';
 
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
 var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
+  Image,
   Text,
   View,
 } = React;
 
 var AwesomeProject = React.createClass({
   render: function() {
+    var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text>{movie.title}</Text>
+        <Text>{movie.year}</Text>
+        <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
       </View>
     );
   }
@@ -47,6 +46,10 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
   },
 });
 
