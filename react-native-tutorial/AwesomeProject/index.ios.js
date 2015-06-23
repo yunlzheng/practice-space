40c2a41a1eedf1d3be6331689da03f5413f4a9ba
yunl.zheng <yunl.zheng@gmail.com>
2015-06-23 13:46:49 +0800

    Fetching real data

--- react-native-tutorial/AwesomeProject/index.ios.js
+++ react-native-tutorial/AwesomeProject/index.ios.js
@@ -8,6 +8,8 @@ var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 var React = require('react-native');
 var {
   AppRegistry,
@@ -18,19 +20,58 @@ var {
 } = React;
 
 var AwesomeProject = React.createClass({
-  render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
+  getInitialState: function() {
+   return {
+     movies: null,
+   };
+  },
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
+   fetch(REQUEST_URL)
+     .then((response) => response.json())
+     .then((responseData) => {
+       this.setState({
+         movies: responseData.movies,
+       });
+     })
+     .done();
+ },
+ renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail} />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year} >{movie.year}</Text>
-        </View>
+        <Text>
+          Loading movies...
+        </Text>
       </View>
     );
+  },
+  renderMovie: function(movie) {
+   return (
+     <View style={styles.container}>
+       <Image
+         source={{uri: movie.posters.thumbnail}}
+         style={styles.thumbnail}
+       />
+       <View style={styles.rightContainer}>
+         <Text style={styles.title}>{movie.title}</Text>
+         <Text style={styles.year}>{movie.year}</Text>
+       </View>
+     </View>
+   );
+ },
+  render: function() {
+
+
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+
+    return this.renderMovie(movie);
+
   }
 });
 
